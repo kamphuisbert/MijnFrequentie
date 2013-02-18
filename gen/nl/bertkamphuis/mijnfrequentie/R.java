16368e8783e8c6f43ab4cdddddb22c3fdb30e1cb
Bert Kamphuis <kamphuisbert@hotmail.com>
2013-02-18 21:24:49 +0100

    added menu bar

--- gen/nl/bertkamphuis/mijnfrequentie/R.java
+++ gen/nl/bertkamphuis/mijnfrequentie/R.java
@@ -21,10 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_help=0x7f020000;
+        public static final int action_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int help=0x7f090002;
+        public static final int settings=0x7f090001;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +40,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
+        public static final int help=0x7f060003;
     }
     public static final class style {
         /** 
--- src/nl/bertkamphuis/mijnfrequentie/MainActivity.java
+++ src/nl/bertkamphuis/mijnfrequentie/MainActivity.java
@@ -1,8 +1,10 @@
 package nl.bertkamphuis.mijnfrequentie;
 
+import android.content.Intent;
 import android.os.Bundle;
 //import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import nl.bertkamphuis.mijnfrequentie.R;
 import org.apache.cordova.DroidGap;
 
@@ -11,6 +13,8 @@ public class MainActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.init();
+        this.registerForContextMenu(this.appView);
         super.loadUrl("file:///android_asset/www/index.html");
     }
 
@@ -20,6 +24,21 @@ public class MainActivity extends DroidGap {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+        case R.id.settings:
+            this.startActivity(new Intent(android.provider.Settings.ACTION_SETTINGS));
+            return true;
+        case R.id.help:
+            this.appView.sendJavascript("navigator.notification.alert('No help')");
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
     }
     
 }
